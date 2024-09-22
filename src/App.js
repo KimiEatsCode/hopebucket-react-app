63f54b46ab72c2f71ac35508b37ebcff5329b3c5
Kimi Rettig <sarahkimirettig@gmail.com>
2024-09-22 13:36:41 -0400

    x

--- src/App.js
+++ src/App.js
@@ -1,10 +1,44 @@
-import React from "react";
-import Bucket from "./components/Bucket";
+import * as React from "react";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/css/bootstrap.css";
+
+import { Outlet } from "react-router-dom";
+// import TopMenu from "./components/TopMenu";
+// import Container from "react-bootstrap/Container";
+import Nav2 from "./components/Nav2";
+
+
+import TopMenu from "./components/TopMenu";
+import Container from "react-bootstrap/Container";
+
+
+
+// const containerStyles = {
+//   display: "flex",
+//   flexDirection: "column",
+//   minHeight: "80vh",
+// };
+
 
 function App() {
+
   return (
     <>
-      <Bucket></Bucket>
+  <h1 className="logoName mb-4">Hope Bucket</h1>
+
+
+<TopMenu></TopMenu>
+      {/* <Container style={containerStyles}> */}
+<Container>
+    <App />
+
+    <Outlet />
+            <Nav2></Nav2>
+            </Container>
+
+
+
     </>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,46 +1,48 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
-import "bootstrap/dist/css/bootstrap.min.css";
-import "bootstrap/dist/css/bootstrap.css";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import List from "./components/List";
-import Nav2 from "./components/Nav2";
-import { Outlet } from "react-router-dom";
-import Container from "react-bootstrap/Container";
-import Bucket from "./components/Bucket";
+import * as React from "react";
+import * as ReactDOM from "react-dom/client";
 import App from "./App";
-import ListContext from "./ListContext";
+
+import Bucket from "./components/Bucket";
 import Newsletter from "./components/Newsletter";
-import TopMenu from "./components/TopMenu";
+import List from "./components/List";
+import ListContext from "./ListContext";
 
-const containerStyles = {
-  display: "flex",
-  flexDirection: "column",
-  minHeight: "80vh",
-};
 
-ReactDOM.render(
+
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+  },
+  {
+    path: '/bucket',
+    element: <Bucket />,
+  },
+
+  {
+    path: '/list',
+    element: <List />,
+  },
+  {
+    path: '/newsletter',
+    element: <Newsletter />,
+  },
+  {
+    path: '/list',
+    element: <list />,
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <ListContext>
-      <BrowserRouter>
-      <TopMenu></TopMenu>
-        <Container style={containerStyles}>
 
-            <h1 className="logoName mb-4">Hope Bucket</h1>
+<RouterProvider router={router} />;
+</ListContext>
 
-          <Routes>
-            <Route path="/" element={<App />} />
-            <Route path="bucket" element={<Bucket />} />
-            <Route path="list" element={<List />} />
-            <Route path="newsletter" element={<Newsletter />} />
-          </Routes>
-          <Nav2></Nav2>
-        </Container>
-      </BrowserRouter>
+  </React.StrictMode>
 
-      <Outlet />
-    </ListContext>
-  </React.StrictMode>,
-  document.getElementById("root")
 );
