e4167dc8237d548d992eda9154dbc210760a97b3
Kimi Rettig <sarahkimirettig@gmail.com>
2024-09-22 14:25:36 -0400

    Revert "x"
    This reverts commit 63f54b46ab72c2f71ac35508b37ebcff5329b3c5.

--- src/App.js
+++ src/App.js
@@ -1,44 +1,10 @@
-import * as React from "react";
-import "./App.css";
-import "bootstrap/dist/css/bootstrap.min.css";
-import "bootstrap/dist/css/bootstrap.css";
-
-import { Outlet } from "react-router-dom";
-// import TopMenu from "./components/TopMenu";
-// import Container from "react-bootstrap/Container";
-import Nav2 from "./components/Nav2";
-
-
-import TopMenu from "./components/TopMenu";
-import Container from "react-bootstrap/Container";
-
-
-
-// const containerStyles = {
-//   display: "flex",
-//   flexDirection: "column",
-//   minHeight: "80vh",
-// };
-
+import React from "react";
+import Bucket from "./components/Bucket";
 
 function App() {
-
   return (
     <>
-  <h1 className="logoName mb-4">Hope Bucket</h1>
-
-
-<TopMenu></TopMenu>
-      {/* <Container style={containerStyles}> */}
-<Container>
-    <App />
-
-    <Outlet />
-            <Nav2></Nav2>
-            </Container>
-
-
-
+      <Bucket></Bucket>
     </>
   );
 }
