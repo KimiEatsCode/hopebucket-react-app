de8661faec61fdf876c44aea22ebfeace7b87555
Kimi Rettig <sarahkimirettig@gmail.com>
2024-10-15 15:02:04 -0400

    added new npm pkg js-cookie; function to remove cookies after minute test

--- src/ListContext.js
+++ src/ListContext.js
@@ -1,7 +1,34 @@
 import React, { createContext } from "react";
 import { useLocalStorage } from "./hooks/useLocalStorageReceipe";
+import Cookies from 'js-cookie';
 
 export const Context = createContext();
+
+function clearCookiesForNextDay() {
+  const cookies = Cookies.get();
+
+  for (const cookieName in cookies) {
+    const cookie = Cookies.get(cookieName);
+    if (cookie) {
+      // Set the cookie to expire at the end of the current day
+      const expirationDate = new Date();
+      expirationDate.setHours(23, 59, 59, 999);
+      Cookies.set(cookieName, cookie, { expires: expirationDate });
+    }
+  }
+}
+
+// clearCookiesForNextDay();
+
+function clearCookiesForNextMinute() {
+  const cookies = Cookies.get();
+
+  for (const cookieName in cookies) {
+    Cookies.remove(cookieName, { path: '/' }); // Set the path to clear cookies across the entire domain
+  }
+}
+
+clearCookiesForNextMinute()
 
 function ListContext({ children }) {
   const [list, setList] = useLocalStorage("hopeList", []);
