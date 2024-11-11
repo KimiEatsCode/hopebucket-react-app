1cebb5edbfdccd5052992915f32a8dc09bbdb10f
Kimi Rettig <sarahkimirettig@gmail.com>
2024-11-11 15:54:46 -0500

    add hope item field as textarea so can type more than one line

--- src/components/Nav2.js
+++ src/components/Nav2.js
@@ -140,7 +140,7 @@ useEffect(() => {
         <Offcanvas.Body className="no-wrap">
           <Row mb={3}>
             <Col>
-              <Form.Control
+              <Form.Control as="textarea" rows={3}
                 placeholder="What is something positive that happened or something you did that gives you hope today?"
                 size="lg"
                 ref={fieldFocus}
@@ -172,29 +172,7 @@ useEffect(() => {
           <i className="bi bi-box-arrow-left"></i>
 <div style={navStyles}>
            { toggleAlignNav ? (
-            <Link onClick={handleNavAlign}>
-              <button type="button" className="btn btn-primary">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="44"
-                  height="44"
-                  fill="currentColor"
-                  className="bi bi-box-arrow-left"
-                  viewBox="0 0 16 16"
-                >
-                  <path
-                    fillRule="evenodd"
-                    d="M6 12.5a.5.5 0 0 0 .5.5h8a.5.5 0 0 0 .5-.5v-9a.5.5 0 0 0-.5-.5h-8a.5.5 0 0 0-.5.5v2a.5.5 0 0 1-1 0v-2A1.5 1.5 0 0 1 6.5 2h8A1.5 1.5 0 0 1 16 3.5v9a1.5 1.5 0 0 1-1.5 1.5h-8A1.5 1.5 0 0 1 5 12.5v-2a.5.5 0 0 1 1 0z"
-                  />
-                  <path
-                    fillRule="evenodd"
-                    d="M.146 8.354a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L1.707 7.5H10.5a.5.5 0 0 1 0 1H1.707l2.147 2.146a.5.5 0 0 1-.708.708z"
-                  />
-                </svg>
-              </button>
-            </Link>
-          ) : (
-            <Link onClick={handleNavAlign}>
+              <Link onClick={handleNavAlign}>
               <button type="button" className="btn btn-primary">
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
@@ -215,6 +193,29 @@ useEffect(() => {
                 </svg>
               </button>
             </Link>
+
+          ) : (
+            <Link onClick={handleNavAlign}>
+            <button type="button" className="btn btn-primary">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="44"
+                height="44"
+                fill="currentColor"
+                className="bi bi-box-arrow-left"
+                viewBox="0 0 16 16"
+              >
+                <path
+                  fillRule="evenodd"
+                  d="M6 12.5a.5.5 0 0 0 .5.5h8a.5.5 0 0 0 .5-.5v-9a.5.5 0 0 0-.5-.5h-8a.5.5 0 0 0-.5.5v2a.5.5 0 0 1-1 0v-2A1.5 1.5 0 0 1 6.5 2h8A1.5 1.5 0 0 1 16 3.5v9a1.5 1.5 0 0 1-1.5 1.5h-8A1.5 1.5 0 0 1 5 12.5v-2a.5.5 0 0 1 1 0z"
+                />
+                <path
+                  fillRule="evenodd"
+                  d="M.146 8.354a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L1.707 7.5H10.5a.5.5 0 0 1 0 1H1.707l2.147 2.146a.5.5 0 0 1-.708.708z"
+                />
+              </svg>
+            </button>
+          </Link>
           )}
 
           {showNewList && (
