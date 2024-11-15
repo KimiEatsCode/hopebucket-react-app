c3414b0b9b076c564a87e9d49e14ec8f1c2ab0d5
Kimi Rettig <sarahkimirettig@gmail.com>
2024-11-15 13:09:48 -0500

    added component style just to remove underline on link; importing lottie from hooks folder
    need specific style to remove text decoration because setting text to relative positioning causes underline on links to be not under but behind text

--- src/components/Bucket.js
+++ src/components/Bucket.js
@@ -4,7 +4,7 @@ import { ListContext } from "../contexts/ListContext";
 // import { ExpContext } from "../contexts/ExpContext";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
-import { LottieControl } from "./LottieControl";
+import LottieControl from "../hooks/LottieControl";
 
 function Bucket() {
   const listContext = useContext(ListContext);
@@ -14,17 +14,20 @@ function Bucket() {
   // let expDate = expContext.expDate;
 
   const confettiStyle = {
-    display: 'block',
-    position:'absolute',
-    top:'0',
+    display: "block",
+    position: "absolute",
+    top: "0",
     // border:'1px solid red',
-    margin: '10px auto',
+    margin: "10px auto",
   };
 
-
+  //need specific style to remove text decoration because setting text to relative positioning causes underline on links to be not under but behind text
+  const linkRemoveUnderline = {
+    textDecoration: "none",
+  };
   return (
     <>
-     <Row className="text-center mt-4 mb-2 mx-auto">
+      <Row className="text-center mt-4 mb-2 mx-auto">
         <Col>
           <h3>
             {totalHope === 3 ? "Congrats! You filled your hope bucket!" : ""}
@@ -32,14 +35,14 @@ function Bucket() {
           </h3>
         </Col>
       </Row>
-       <Row className="mx-auto">
+      <Row className="mx-auto">
         <Col>
-        <div style={confettiStyle}>
-        <LottieControl></LottieControl>
-        </div>
-          <Link to="/list">
+          <div style={confettiStyle}>
+            <LottieControl></LottieControl>
+          </div>
+          <Link to="/list" style={linkRemoveUnderline}>
             <div className="bucketIcon">
-            <span className="hopeCount">{totalHope} of 3</span>
+              <span className="hopeCount">{totalHope} of 3</span>
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 width="60%"
@@ -47,14 +50,11 @@ function Bucket() {
                 fill="#000"
                 className="bi bi-bucket"
                 viewBox="0 0 16 16"
-              >
-              </svg>
-
+              ></svg>
             </div>
           </Link>
         </Col>
       </Row>
-
     </>
   );
 }
