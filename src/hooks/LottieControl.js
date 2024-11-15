81cfec1a5040d6ddcd3277d44c42edf1d50e4582
Kimi Rettig <sarahkimirettig@gmail.com>
2024-11-15 15:28:49 -0500

    x

--- src/hooks/LottieControl.js
+++ src/hooks/LottieControl.js
@@ -1,36 +1,32 @@
 import React, { useEffect, useState, useContext, useRef } from "react";
 import Lottie from "lottie-react";
 import confettiAnimation from "../images/confetti-lottie.json";
-import { ListContext } from '../contexts/ListContext';
-
+import { ListContext } from "../contexts/ListContext";
 
 const LottieControl = () => {
+  const lottieRef = useRef();
 
-const lottieRef = useRef();
+  const listContext = useContext(ListContext);
+  const list = listContext.list;
+  let totalHope = listContext.list.length;
 
-const listContext = useContext(ListContext);
-const list = listContext.list;
-let totalHope = listContext.list.length;
+  useEffect(() => {
+    if (totalHope === 3) {
+      console.log("totalHope" + list);
+      lottieRef.current.play();
+    } else {
+      lottieRef.current.stop();
+    }
+  }, [totalHope, list]);
 
-useEffect(() => {
-  if (totalHope === 3) {
-    console.log('totalHope' + list);
-    lottieRef.current.play();
-  } else {
-    lottieRef.current.stop();
-
-  }
-}, [totalHope, list]);
-
-const confettistyle = {
-  position:"absolute",
-  zIndex:"0",
-  left:"0",
-  top:"0",
-  height: "100%",
-  width: "100%",
-  border:"1px solid red"
-};
+  const confettistyle = {
+    position: "absolute",
+    zIndex: "0",
+    left: "0",
+    top: "0",
+    height: "100%",
+    width: "100%",
+  };
 
   return (
     <Lottie
