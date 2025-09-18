2b10f37d9430fb8e40c4af12714e8a59afc62b12
Sarah Kimi Rettig <sarahkimirettig@gmail.com>
2025-09-18 13:05:18 -0400

    Created new lottie component for nav msg

---
+++ src/hooks/navMsgControl.js
@@ -0,0 +1,51 @@
+import React, { useEffect, useContext, useRef } from "react";
+import Lottie from "lottie-react";
+import bucketAnimation from "../images/bucket-lottie.json";
+import { ListContext } from "../contexts/ListContext";
+
+const LottieControl = () => {
+  const lottieRef = useRef();
+
+  const listContext = useContext(ListContext);
+  const list = listContext.list;
+  let totalHope = listContext.list.length;
+
+  useEffect(() => {
+    if (totalHope === 0) {
+      console.log("totalHope" + list);
+      lottieRef.current.goToAndStop(10, true);
+    } else if (totalHope === 1) {
+      console.log("totalHope" + list);
+      lottieRef.current.goToAndStop(20, true);
+    } else if (totalHope === 2) {
+      console.log("totalHope" + list);
+      lottieRef.current.goToAndStop(55, true);
+    } else if (totalHope === 3) {
+      console.log("totalHope" + list);
+      lottieRef.current.playSegments(55, 100);
+    } else {
+      lottieRef.current.stop();
+    }
+  }, [totalHope, list]);
+
+  const bucketstyle = {
+    position: "absolute",
+    zIndex: "-1",
+    left: "0",
+    top: "170px",
+    height: "70%",
+    width: "100%",
+  };
+
+  return (
+    <Lottie
+      lottieRef={lottieRef}
+      autoPlay={false}
+      loop={false}
+      animationData={bucketAnimation}
+      style={bucketstyle}
+    />
+  );
+};
+
+export default LottieControl;
