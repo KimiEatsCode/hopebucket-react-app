62205106f9121c848560b219ea0d6b0249357db2
Sarah Kimi Rettig <sarahkimirettig@gmail.com>
2025-10-04 17:13:15 -0400

    Return to start

--- src/App.js
+++ src/App.js
@@ -1,10 +1,42 @@
 import React from "react";
+import { BrowserRouter, Routes, Route, Outlet, Link } from "react-router-dom";
+import Container from "react-bootstrap/Container";
+//components
+import List from "./components/List";
+import Nav2 from "./components/Nav2";
 import Bucket from "./components/Bucket";
+
+//context
+import { ListContextProvider } from "./contexts/ListContext";
+import { ExpContextProvider } from "./contexts/ExpContext";
 
 function App() {
   return (
     <>
-      <Bucket></Bucket>
+  
+  <React.StrictMode>
+    <ExpContextProvider>
+      <ListContextProvider>
+        <BrowserRouter>
+    
+          <Container>
+            <Link to="/">
+              <h1 className="logoName mb-2">HopeBucket</h1>
+            </Link>
+
+            <Routes>
+              <Route path="/" element={<Bucket />} />
+              <Route path="/bucket" element={<Bucket />} />
+              <Route path="/list" element={<List />} />
+            </Routes>
+
+            <Nav2></Nav2>
+          </Container>
+        </BrowserRouter>
+        <Outlet />
+      </ListContextProvider>
+    </ExpContextProvider>
+  </React.StrictMode>,
     </>
   );
 }
