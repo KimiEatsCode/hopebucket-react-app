308be750b03b4c67005490e20f0b312b2ab1a0bd
Sarah Kimi Rettig <sarahkimirettig@gmail.com>
2025-10-04 16:36:08 -0400

    Revert "Moved code from index js to App js; Removed react bootstrap code"
    This reverts commit 204214a00c3e1bbc7a3e941a449f268d026c9643.

--- src/App.js
+++ src/App.js
@@ -1,48 +1,10 @@
 import React from "react";
-import { BrowserRouter, Routes, Route, Outlet, Link } from "react-router-dom";
-//css
-
-import "./styles/index.css";
-// I use this React Bootstrap for grid https://react-bootstrap.netlify.app/docs/layout/grid/
-//bootstrap react components
-import "bootstrap-icons/font/bootstrap-icons.css";
-//components
-import List from "./components/List";
-import Nav from "./components/Nav";
 import Bucket from "./components/Bucket";
-
-//context
-import { ListContextProvider } from "./contexts/ListContext";
-import { ExpContextProvider } from "./contexts/ExpContext";
 
 function App() {
   return (
-    <>      
-    <ExpContextProvider>
-      <ListContextProvider>
-        <BrowserRouter>
-
-          <div className="container">
-               <div class="row"  xs={2} md={4} lg={6}>
-                     
-            <Link to="/">
-              <h1 className="logoName mb-2 mt-4">HopeBucket</h1>
-            </Link>
-
-            <Routes>
-              <Route path="/" element={<Bucket/>} />
-              <Route path="/bucket" element={<Bucket />} />
-              <Route path="/list" element={<List />} />
-            </Routes>
-         
-        </div>
-
-            <Nav></Nav>
-          </div>
-        </BrowserRouter>
-        <Outlet />
-      </ListContextProvider>
-    </ExpContextProvider>
+    <>
+      <Bucket></Bucket>
     </>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,7 +1,47 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter, Routes, Route, Outlet, Link } from "react-router-dom";
+//css
+import "./styles/index.css";
+//bootstrap
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/css/bootstrap.css";
+import Container from "react-bootstrap/Container";
+//components
+import List from "./components/List";
+import Nav2 from "./components/Nav2";
+import Bucket from "./components/Bucket";
+import App from "./App";
+import TopMenu from "./components/TopMenu";
 
-import ReactDOM from 'react-dom/client';
-import App from './App';
+//context
+import { ListContextProvider } from "./contexts/ListContext";
+import { ExpContextProvider } from "./contexts/ExpContext";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+ReactDOM.render(
+  <React.StrictMode>
+    <ExpContextProvider>
+      <ListContextProvider>
+        <BrowserRouter>
+          <TopMenu></TopMenu>
 
-root.render(<App />);
+          <Container>
+            <Link to="/">
+              <h1 className="logoName mb-2">HopeBucket</h1>
+            </Link>
+
+            <Routes>
+              <Route path="/" element={<App />} />
+              <Route path="/bucket" element={<Bucket />} />
+              <Route path="/list" element={<List />} />
+            </Routes>
+
+            <Nav2></Nav2>
+          </Container>
+        </BrowserRouter>
+        <Outlet />
+      </ListContextProvider>
+    </ExpContextProvider>
+  </React.StrictMode>,
+  document.getElementById("root")
+);
