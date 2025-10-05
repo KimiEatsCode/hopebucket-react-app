8a7fb21608ed1c5d3d6b30a6f05912e3f95d0dda
Sarah Kimi Rettig <sarahkimirettig@gmail.com>
2025-10-05 18:24:36 -0400

    x

--- src/components/List.js
+++ src/components/List.js
@@ -168,7 +168,24 @@ if(document.getElementById('copyButton') != null) {
                       className="alignRightX"
                       key={item.id}
                       variant="dark"
-                    ></div>
+                    ></div> <button
+                      className="closeX btn "
+                      onClick={() => {
+                        deleteItem(item.id);
+                      }}
+                    >
+                      {" "}
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="16"
+                        height="16"
+                        fill="currentColor"
+                        className="bi bi-x-lg"
+                        viewBox="0 0 16 16"
+                      >
+                        <path d="M2.146 2.854a.5.5 0 1 1 .708-.708L8 7.293l5.146-5.147a.5.5 0 0 1 .708.708L8.707 8l5.147 5.146a.5.5 0 0 1-.708.708L8 8.707l-5.146 5.147a.5.5 0 0 1-.708-.708L7.293 8 2.146 2.854Z" />
+                      </svg>
+                    </button>
                     <div className="hopeItem">{item.value}</div>
                   </ListGroup.Item>
                 );
